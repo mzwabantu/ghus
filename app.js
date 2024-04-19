ccb3d63f78fd1fa559955526fa3c076221d51b3d
Mzwabantu <web.mzwwwa@gmail.com>
2024-04-19 09:32:19 +0200

    use env file

--- app.js
+++ app.js
@@ -2,124 +2,104 @@ import { Octokit } from "@octokit/core";
 import {notFoundTemplate, searchTemplate, userTemplate, repoTemplate, skeleton} from "./templates.js";
 
 document.addEventListener('DOMContentLoaded', function () {
-    
-    const xhr = new XMLHttpRequest();
+    const octokit = new Octokit({ auth: "you_github_token_here"});
+    const searchInput = document.getElementById("search");
+    const searchResults = document.getElementById("searchResults");
+    const themeSwitcher = document.getElementById("switch");
+    const userResults = document.getElementById("userResults");
+    const body = document.querySelector('body');
+    let prevUserResults = [];
+    let searchTimeout;
 
-    // Make sure config.json file is not exposed
-    xhr.open('GET', 'config.json', true); 
+    // Theme
+    const switchTheme = (arg) => {
+        body.classList[ arg ? 'remove' : 'add']("dark");
+        body.classList[ arg ? 'add' : 'remove']("light");
+    }
+    themeSwitcher.addEventListener('change', function() {switchTheme(this.checked)});
+    const prefersDark = window.matchMedia('(prefers-color-scheme: dark)').matches;
+    themeSwitcher.checked = !prefersDark; 
+    switchTheme(!prefersDark);
 
-    // I hide it prod with approach, where private is not accessible to public, 
-    // xhr.open('GET', '/private/read_config.php', true); 
-    // you could use environment variables aswell
-
-    xhr.onload = function () {
-        if (xhr.status === 200) {
-            const config = JSON.parse(xhr.responseText);
-            const githubToken = config.githubToken;
-            const octokit = new Octokit({ auth: githubToken });
-
-            const searchInput = document.getElementById("search");
-            const searchResults = document.getElementById("searchResults");
-            const themeSwitcher = document.getElementById("switch");
-            const userResults = document.getElementById("userResults");
-            const body = document.querySelector('body');
-            let prevUserResults = [];
-            let searchTimeout;
-
-            // Theme
-            const switchTheme = (arg) => {
-                body.classList[ arg ? 'remove' : 'add']("dark");
-                body.classList[ arg ? 'add' : 'remove']("light");
-            }
-            themeSwitcher.addEventListener('change', function() {switchTheme(this.checked)});
-            const prefersDark = window.matchMedia('(prefers-color-scheme: dark)').matches;
-            themeSwitcher.checked = !prefersDark; 
-            switchTheme(!prefersDark);
-
-            // User Data
-            const fetchUserData = async (username) => {
-                try {
-                    const response = await octokit.request('GET /users/{username}', {
-                        username: username
-                    });
-                    const data = response.data;
-
-                    // Fetch user repositories
-                    const repoResponse = await octokit.request('GET /users/{username}/repos', {
-                        username: username
-                    });
-                    const repoData = repoResponse.data;
-
-                    // Display Results
-                    const repoHTML = repoData.map(repo => repoTemplate(repo)).join('');
-                    userResults.innerHTML = userTemplate(data);
-                    document.getElementById("repoResults").innerHTML = repoHTML;
-                    document.getElementById("repoResults").classList.add('border-bt');
-                } catch (error) {
-                    console.error('Error fetching user data:', error);
-                }
-            }
-            const showResults = (show = true) =>  searchResults.classList[show ? 'add' : 'remove']("show");
-
-            // User Events
-            searchInput.addEventListener('input', async function() {
-                clearTimeout(searchTimeout);
-
-                // Get User Input
-                const search = this.value.trim();
-                if (search === "") {
-                    showResults(false);
-                    searchResults.innerHTML = ""; 
-                    return;
-                }
-
-                // Make Request
-                searchTimeout = setTimeout( async () => {
-                    try {
-                        const response = await octokit.request('GET /search/users', {
-                            q: search
-                        });
-                        
-                        const users = response.data.items;
-                        prevUserResults = users;
-
-                        if (users.length > 0) {
-                            const dropdownHTML = users.map(user => searchTemplate(user)).join('');
-                            searchResults.innerHTML = dropdownHTML;
-                            showResults();
-                        } else {
-                            searchResults.innerHTML = notFoundTemplate('No users found');
-                            showResults();
-                        }
-                    } catch (error) {
-                        console.error('Error fetching data:', error);
-                    }
-                }, 600)
-
+    // User Data
+    const fetchUserData = async (username) => {
+        try {
+            const response = await octokit.request('GET /users/{username}', {
+                username: username
             });
+            const data = response.data;
 
-            searchResults.addEventListener('click', function(event) {
-                showResults(false);
-                const target = event.target;
-                
-                if (target.classList.contains('list-group-item')) {
-                    const username = target.getAttribute('data-username');
-                    userResults.innerHTML = skeleton();
-                    fetchUserData(username);
-                }
+            // Fetch user repositories
+            const repoResponse = await octokit.request('GET /users/{username}/repos', {
+                username: username
             });
+            const repoData = repoResponse.data;
 
-            // Show Previous Search Results
-            document.addEventListener( "click", (event) => {
-                const target = event.target;
-                const isSearch = target.classList.contains('form-control');
-                
-                if (isSearch && prevUserResults.length) showResults();
-                else showResults(false);
-            }); 
-        } else {
-            console.error('Failed to load config.json:', xhr.statusText);
+            // Display Results
+            const repoHTML = repoData.map(repo => repoTemplate(repo)).join('');
+            userResults.innerHTML = userTemplate(data);
+            document.getElementById("repoResults").innerHTML = repoHTML;
+            document.getElementById("repoResults").classList.add('border-bt');
+        } catch (error) {
+            console.error('Error fetching user data:', error);
         }
-    };
-    xhr.send();
+    }
+    const showResults = (show = true) =>  searchResults.classList[show ? 'add' : 'remove']("show");
+
+    // User Events
+    searchInput.addEventListener('input', async function() {
+        clearTimeout(searchTimeout);
+
+        // Get User Input
+        const search = this.value.trim();
+        if (search === "") {
+            showResults(false);
+            searchResults.innerHTML = ""; 
+            return;
+        }
+
+        // Make Request
+        searchTimeout = setTimeout( async () => {
+            try {
+                const response = await octokit.request('GET /search/users', {
+                    q: search
+                });
+                
+                const users = response.data.items;
+                prevUserResults = users;
+
+                if (users.length > 0) {
+                    const dropdownHTML = users.map(user => searchTemplate(user)).join('');
+                    searchResults.innerHTML = dropdownHTML;
+                    showResults();
+                } else {
+                    searchResults.innerHTML = notFoundTemplate('No users found');
+                    showResults();
+                }
+            } catch (error) {
+                console.error('Error fetching data:', error);
+            }
+        }, 600)
+
+    });
+
+    searchResults.addEventListener('click', function(event) {
+        showResults(false);
+        const target = event.target;
+        
+        if (target.classList.contains('list-group-item')) {
+            const username = target.getAttribute('data-username');
+            userResults.innerHTML = skeleton();
+            fetchUserData(username);
+        }
+    });
+
+    // Show Previous Search Results
+    document.addEventListener( "click", (event) => {
+        const target = event.target;
+        const isSearch = target.classList.contains('form-control');
+        
+        if (isSearch && prevUserResults.length) showResults();
+        else showResults(false);
+    }); 
 });
--- webpack.config.js
+++ webpack.config.js
@@ -1,7 +1,8 @@
 const path = require('path');
+const Dotenv = require('dotenv-webpack');
 
 module.exports = {
-    mode: 'production',
+    mode: 'development', // or 'production'
     entry: './app.js', 
     output: {
         path: path.resolve(__dirname, 'dist'), 
@@ -20,5 +21,18 @@ module.exports = {
                 }
             }
         ]
-    }
+    },
+    resolve: {
+        fallback: {
+            "crypto": require.resolve("crypto-browserify"),
+            "buffer": require.resolve("buffer/"),
+            "stream": require.resolve("stream-browserify"),
+            "path": require.resolve("path-browserify"),
+            "os": require.resolve("os-browserify/browser"),
+            "vm": require.resolve("vm-browserify")
+        }
+    },
+    plugins: [
+        new Dotenv(),
+    ],
 };
